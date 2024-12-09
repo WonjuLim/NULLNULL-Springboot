ce716eca175a3fda6dcffc9ba5589e0c0068c0c2
WonjuLim <107308782+WonjuLim@users.noreply.github.com>
2024-12-09 16:43:02 +0900

    로그인/로그아웃

--- festival/src/main/java/com/teamproject/festival/MainPageController.java
+++ festival/src/main/java/com/teamproject/festival/MainPageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainPageController {
     @GetMapping("/")
     public String main() {
-        return "/main/index";
+        return "/index";
     }
 }
